260995c93af67ae001aab8c989c1e39dd143ec8f
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-11-14 20:02:10 +0100

    [FEAT] HomePage

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden"></br>
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptYa is a modern AI prompting tool. You can create, share, and find
+        a lot of creative prompts. It's 100% free.
+      </p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
