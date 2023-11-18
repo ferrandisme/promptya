faa42be6039961e6927faaded1b953089cb03297
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-11-18 18:59:29 +0100

    [FIX]

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,5 +1,5 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
   try {
@@ -10,6 +10,6 @@ export const GET = async (request) => {
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response("Failed to retrieve prompts", { status: 500 });
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
