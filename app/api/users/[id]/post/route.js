b341dcc7d8a08626d6681204824b0b055a5d3f19
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-11-18 19:02:12 +0100

    [FIX]

--- app/api/users/[id]/post/route.js
+++ app/api/users/[id]/post/route.js
@@ -1,12 +1,14 @@
-import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
   try {
     //lambda function, stop after ending. needs each time to connect
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate("creator");
+    const prompts = await Prompt.find({ creator: params.id }).populate(
+      "creator"
+    );
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
