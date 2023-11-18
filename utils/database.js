d34c97085f6581dc9108b713a7c9c135ac4d5068
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-11-18 20:11:43 +0100

    [OTHER] Removed console.log

--- utils/database.js
+++ utils/database.js
@@ -6,14 +6,12 @@ export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (isConnected) {
-    console.log("MongoDB is already connected");
+    //console.log("MongoDB is already connected");
     return;
   }
 
   try {
-    console.log(
-      "-------------\n\nCONECTANDO mongo a " + process.env.MONGODB_URI
-    );
+    console.log("-------------\nConecting MongoDB " + process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
