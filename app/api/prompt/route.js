ab14ed7e67cd1e2402bb85b1c285d930ee735e3b
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-11-23 18:25:34 +0100

    [FIX] Cache home

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,19 +1,21 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const GET = async (request) => {
+export const GET = async (request, response) => {
   try {
     //lambda function, stop after ending. needs each time to connect
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    res.setHeader(
+    response.setHeader(
       "Cache-Control",
       "no-cache, no-store, max-age=0, must-revalidate"
     );
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
+    return new Response("Failed to fetch all prompts: " + error, {
+      status: 500,
+    });
   }
 };
