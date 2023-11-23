d5f441aec15e9370834f45794f73afcfa22c1338
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-11-23 23:06:49 +0100

    [FIX] dynamic force

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,22 +1,16 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+export const dynamic = "force-dynamic";
 export const GET = async (request, response) => {
   try {
     //lambda function, stop after ending. needs each time to connect
     await connectToDB();
 
-    const prompts = await Prompt.find({}).populate("creator").lean().exec();
-
-    const headers = new Headers();
-    headers.append(
-      "Cache-Control",
-      "no-cache, no-store, max-age=0, must-revalidate"
-    );
+    const prompts = await Prompt.find({}).populate("creator");
 
     return new Response(JSON.stringify(prompts), {
       status: 200,
-      headers: headers,
     });
   } catch (error) {
     return new Response("Failed to fetch all prompts: " + error, {
