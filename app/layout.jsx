8418cf5bea6850d4d0d3e6bda0eba6370aeb1d04
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-12-04 19:21:27 +0100

    [FEAT] Vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
+import { Analytics } from "@vercel/analytics/react";
+
 export const metadata = {
   title: "PromptYa",
   description: "Discover & share AI Prompts",
@@ -17,6 +19,7 @@ const RootLayout = ({ children }) => (
         <main className="app">
           <Nav />
           {children}
+          <Analytics />
         </main>
       </Provider>
     </body>
